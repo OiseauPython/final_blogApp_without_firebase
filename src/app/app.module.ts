c510222972405cd00824c7f4b26d085be757f3e4
OiseauPython <stankojeremy96@gmail.com>
2019-01-26 14:26:59 +0100

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListPostComponent } from './list-post/list-post.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPostComponent,
+    NewPostComponent,
+    SinglePostComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule
